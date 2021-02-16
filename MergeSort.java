3a470c24d44f0df4895657e0c679df8ab804be97
anghielo <anghielo@yahoo.com>
2021-02-16 04:14:38 -0800

    Update notes

--- MergeSort.java
+++ MergeSort.java
@@ -2,6 +2,15 @@
 // Sorting Algorithms - MergeSort
 // 19 March 2021
 
+/*
+   Implement Merge Sort and Quick Sort on your computer to sort a list of n elements. Carry out a
+   complete test of your algorithms with n = 10,000, 20,000, 50,000, 100,000,...(up to the largest
+   size of n that your computer can handle – You should stop the program for any method if it takes
+   more than 10 minutes to run). Draw a graph to compare the growth rates of these two algorithms.
+*/
+
 public class MergeSort {
-    
+    public static void main(String[] args) {
+        
+    }    
 }
